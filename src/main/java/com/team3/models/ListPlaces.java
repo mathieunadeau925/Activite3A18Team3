f72f25b41026efab0e221228860b4881f584a714
1995046 <1995046@LI-S024-0T14>
2019-11-29 15:07:45 -0500

    Ajout de méthodes pour recuperer le nearbysearch, avec ou sans type de recherche.

--- src/main/java/com/team3/models/ListPlaces.java
+++ src/main/java/com/team3/models/ListPlaces.java
@@ -3,20 +3,20 @@ package com.team3.models;
 import gsonModels.Place;
 import java.util.ArrayList;
 
-public class ListResultsNearbySearch {
-   private ArrayList<Place> listResultsNearbySearch = 
+public class ListPlaces {
+   private ArrayList<Place> listPlaces = 
            new ArrayList<>();
    
    public void addTolList(Place place) {
-       listResultsNearbySearch.add(place);
+       listPlaces.add(place);
    }
    
    public void clearList() {
-       listResultsNearbySearch.clear();
+       listPlaces.clear();
    }
    
    public Place findInList(String place_id) {
-       for (Place p : listResultsNearbySearch) {
+       for (Place p : listPlaces) {
            if (p.getPlace_id().equals(place_id)) {
                return p;
            }
@@ -25,7 +25,7 @@ public class ListResultsNearbySearch {
    }
 
     public void printList() {
-        for(Place p : listResultsNearbySearch) {
+        for(Place p : listPlaces) {
             System.out.println(p.toString());
         }
     }
