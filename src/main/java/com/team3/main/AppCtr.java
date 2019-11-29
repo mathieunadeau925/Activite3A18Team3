d3b68e61345f4fdac1ae75b13147bb1edc940ef6
1995159 <1995159@LI-S024-0T15>
2019-11-29 15:46:34 -0500

    Change my AppCtr

--- src/main/java/com/team3/main/AppCtr.java
+++ src/main/java/com/team3/main/AppCtr.java
@@ -1,6 +1,8 @@
 package com.team3.main;
 
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.team3.util.UtilPlaceDetails;
 import gsonModels.Review;
 import java.util.ArrayList;
@@ -9,10 +11,6 @@ import java.util.ArrayList;
 public class AppCtr {
 
     public static void main(String[] args) {
-       String placeID = "ChIJm9yJF0wayUwRO2PxDSXpH0Q";
-       ArrayList<Review> l = UtilPlaceDetails.getPlaceDetails(placeID);
-        for (Review review : l) {
-            System.out.println(review);
-        }
+
     }
 }
