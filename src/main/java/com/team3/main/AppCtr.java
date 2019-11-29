7e0fef331156f9f6e9e9ffd8b862e5354a6a5468
1995159 <1995159@LI-S024-0T15>
2019-11-29 15:07:27 -0500

    Add methode getPlaceDetails, and extract Reviews

--- src/main/java/com/team3/main/AppCtr.java
+++ src/main/java/com/team3/main/AppCtr.java
@@ -1,6 +1,7 @@
 package com.team3.main;
 
 import com.team3.util.GoogleKey;
+import com.team3.util.UtilPlaceDetails;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.logging.Level;
@@ -16,36 +17,9 @@ import org.apache.http.util.EntityUtils;
 public class AppCtr {
 
     public static void main(String[] args) {
+       String placeID = "ChIJm9yJF0wayUwRO2PxDSXpH0Q";
        
-        long epoch = 1574970764;
-        double lat = 45;
-        double lon = -73;
-        makeCall(lat, lon, epoch, GoogleKey.getGOOGLE_API_KEY());
-    }
-
-    private static void makeCall(double lat, double lon, long epoch, String GOOGLE_API_KEY) {
-//creer votre client HTTP
-        HttpClient client = HttpClientBuilder.create().build();
-//creer votre URL de votre service en specifiant type de connexion HTTP/HTTPS , host et chemin de la API
-        URIBuilder builder = new URIBuilder().setScheme("https").setHost("maps.googleapis.com").setPath("/maps/api/timezone/json");
-        String location = lat + "," + lon;
-//Ajouter a votre URL le paramettre localisation en specifiant la cle et la valeur
-        builder.addParameter("location", location);
-//Ajouter a votre URL le paramettre timestamp en specifiant la cle et la valeur
-        builder.addParameter("timestamp", String.valueOf(epoch));
-//Ajouter a votre URL le paramettre GOOGLE_API_KEY en specifiant la cle et la valeur
-        builder.addParameter("key", GOOGLE_API_KEY);
-        try {
-//Creer la requete Get a partir de votre URL
-            HttpUriRequest request = new HttpGet(builder.build());
-//Executer la requete Get a partir de votre URL
-            HttpResponse execute = client.execute(request);
-//Convertir la reponse a un String
-            String response = EntityUtils.toString(execute.getEntity());
-//afficher la reponse
-            System.out.println(response);
-        } catch (URISyntaxException | IOException ex) {
-            Logger.getLogger(AppCtr.class.getName()).log(Level.SEVERE, null, ex);
-        }
+        UtilPlaceDetails.
+        
     }
 }
--- src/main/java/com/team3/util/UtilPlaceDetails.java
+++ src/main/java/com/team3/util/UtilPlaceDetails.java
@@ -1,5 +1,49 @@
 package com.team3.util;
 
+import com.google.gson.JsonObject;
+import com.team3.main.AppCtr;
+import com.team3.models.PlaceDetails;
+import gsonModels.Review;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+
 public class UtilPlaceDetails {
+    
+    
+    
+    public static ArrayList<PlaceDetails> getPlaceDetails(String placeID){
+        HttpClient client = HttpClientBuilder.create().build();
+        URIBuilder builder = new URIBuilder().setScheme("https").setHost("maps.googleapis.com").setPath("/api/place/details/json");
+        ArrayList<PlaceDetails> listPlaceDetails = new ArrayList<>();
+
+        builder.addParameter("place_id", placeID);
+        builder.addParameter("key", GoogleKey.getGOOGLE_API_KEY());
+        
+        try {
+
+            HttpUriRequest request = new HttpGet(builder.build());
+            HttpResponse execute = client.execute(request);
+            String response = EntityUtils.toString(execute.getEntity());
+            listPlaceDetails=extractJsonArrayReviews(response);
+        } catch (URISyntaxException | IOException ex) {
+            Logger.getLogger(AppCtr.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        return listPlaceDetails;
+    }
+
+    private static ArrayList<PlaceDetails> extractJsonArrayReviews(String response) {
+        //JSONObject reviews = JsonObject.fromObject(response);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
