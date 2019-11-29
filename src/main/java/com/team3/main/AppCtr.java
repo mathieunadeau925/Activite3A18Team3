ee3f0b7cd8388c1fedfe380546ff016655c7134f
1995046 <1995046@LI-S024-0T14>
2019-11-29 16:33:44 -0500

    Ajout de commentaires

--- src/main/java/com/team3/main/AppCtr.java
+++ src/main/java/com/team3/main/AppCtr.java
@@ -8,7 +8,9 @@ import net.sf.json.JSONArray;
 public class AppCtr {
 
     public static void main(String[] args) {
+        //test du service, qui demanderais une latitude, longitude, rayon et type
         JSONArray liste = UtilJsonBuilder.traiterFichierGson("45.52048", "-73.5686184", 1500, "food");
+        
         final GsonBuilder builder = new GsonBuilder();
         final Gson gson = builder.setPrettyPrinting().create();
         String jsonArray = gson.toJson(liste);
